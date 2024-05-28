60c96c46a1824add18a5ff6ade657d270066ee41
Vitor Aguiar <vitoraguiar.df@gmail.com>
2024-05-28 20:54:11 -0300

    basic auth

--- main.go
+++ main.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"net/http"
+
 	"github.com/gin-gonic/gin"
 )
 
@@ -11,8 +13,14 @@ func main() {
 
 func setupRouter() *gin.Engine {
 	router := gin.Default()
-	router.GET("/ping", func(c *gin.Context) {
-		c.String(200, "pong")
+	router.GET("/ping", func(ctx *gin.Context) {
+		ctx.String(http.StatusOK, "pong")
+	})
+	routerAdmin := router.Group("/admin", gin.BasicAuth(gin.Accounts{
+		"vitor": "rotiv",
+	}))
+	routerAdmin.GET("/index", func(ctx *gin.Context) {
+		ctx.String(http.StatusOK, "Admin is here!")
 	})
 	return router
 }
